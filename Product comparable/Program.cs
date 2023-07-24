22c25d87e1a9ff8ff24c253b920291cc75586a57
pankajdaundkar <phd27993@gmail.com>
2023-07-24 13:46:26 +0530

    Add project files.

---
+++ Product comparable/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product_comparable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product Mobile = new Product("Mobile", 12000);
+            Product Laptop = new Product("Laptop", 30000);
+
+            int result = Mobile.CompareTo(Laptop);
+
+            if(result < 0) 
+            {
+                Console.WriteLine("Mobile has less price than Laptop");
+            }
+            else if(result > 0) 
+            {
+                Console.WriteLine("Mobile has more price than Laptop");
+            }
+            else
+            {
+                Console.WriteLine("Mobile and Laptop has same price");
+            }
+        }
+    }
+}
---
+++ Product comparable/product.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product_comparable
+{
+    public class Product:IComparable<Product>
+    {
+        public string Name;
+        public int Price;
+
+        public Product(string name, int price)
+        {
+           this.Name = name;
+            this.Price = price;
+        }
+
+        public int CompareTo(Product other)
+        {
+           if (this.Price < other.Price)
+            {
+                return -1;
+            }
+           else if (this.Price > other.Price)
+            {
+                return 1;
+            }
+           else
+            {
+                return 0;
+            }
+
+        }
+
+    }
+   
+    
+    
+}
